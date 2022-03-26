0c0abd0d032a25f61bff058ae7beecc2c82c1f13
VincentMald <vincentmald@gmail.com>
2022-03-26 17:40:46 +0100

    log added

--- Back/app.js
+++ Back/app.js
@@ -2,19 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
-const {username, password} = require('../config.json')
+const { username, password } = require('../config.json')
 
 const Music = require("./models/Music")
 
 mongoose.connect(`mongodb+srv://${username}:${password}@muizz-bot-jchyj.mongodb.net/Muizz-bot`)
     .then(() => {
         console.log('Database connected')
+        console.log("coucou")
     })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use ((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
@@ -35,7 +36,7 @@ app.post("/api/music", (req, res, next) => {
 })
 
 app.get("/api/music", (req, res, next) => {
-    Music.aggregate([ { $sample: { size: 1 } } ]).then(documents => {
+    Music.aggregate([{ $sample: { size: 1 } }]).then(documents => {
         console.log(documents)
         res.status(200).json({
             message: "OK",
